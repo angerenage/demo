9df5f1a58de5686c97c35431b7d6cb9ff2ad9ddf
angerenage <ange.renage@gmail.com>
2024-05-04 22:42:56 +0200

    Reorganizating geometry files and adding functions for transformation matrix

--- geometry.c
+++ geometry.c
@@ -1,47 +1,102 @@
 #include "geometry.h"
 
+mat4 getIdentity() {
+	mat4 matrix = {0};
+	for (int i = 0; i < 4; i++) {
+		matrix.m[i][i] = 1.0f;
+	}
+	return matrix;
+}
+
 mat4 projectionMatrix(float fov, float aspectRatio, float nearPlane, float farPlane) {
-	mat4 mat = {0};
+	mat4 matrix = {0};
 	
 	float yScale = 1.0f / tan(fov / 2.0f);
 	float xScale = yScale / aspectRatio;
 	float frustumLength = farPlane - nearPlane;
 
-	mat.m[0] = xScale;
-	mat.m[5] = yScale;
-	mat.m[10] = -((farPlane + nearPlane) / frustumLength);
-	mat.m[11] = -1;
-	mat.m[14] = -((2 * nearPlane * farPlane) / frustumLength);
+	matrix.m[0][0] = xScale;
+	matrix.m[1][1] = yScale;
+	matrix.m[2][2] = -((farPlane + nearPlane) / frustumLength);
+	matrix.m[2][3] = -1;
+	matrix.m[3][2] = -((2 * nearPlane * farPlane) / frustumLength);
 	
-	return mat;
+	return matrix;
 }
 
 mat4 viewMatrix(vec3 position, vec3 focus, vec3 up) {
-	vec3 f = normalize(substracteVector(focus, position));
+	vec3 f = normalize(vec3_subtract(focus, position));
 	vec3 r = normalize(crossProduct(f, up));
 	vec3 u = crossProduct(r, f);
 
-	mat4 mat = {0};
-	mat.m[0] = r.x;
-	mat.m[4] = r.y;
-	mat.m[8] = r.z;
-	mat.m[1] = u.x;
-	mat.m[5] = u.y;
-	mat.m[9] = u.z;
-	mat.m[2] = -f.x;
-	mat.m[6] = -f.y;
-	mat.m[10] = -f.z;
-	mat.m[15] = 1;
+	mat4 matrix = {0};
+	matrix.m[0][0] = r.x;
+	matrix.m[1][0] = r.y;
+	matrix.m[2][0] = r.z;
+	matrix.m[0][1] = u.x;
+	matrix.m[1][1] = u.y;
+	matrix.m[2][1] = u.z;
+	matrix.m[0][2] = -f.x;
+	matrix.m[1][2] = -f.y;
+	matrix.m[2][2] = -f.z;
+	matrix.m[3][3] = 1;
 
-	mat.m[12] = -r.x * position.x - r.y * position.y - r.z * position.z;
-	mat.m[13] = -u.x * position.x - u.y * position.y - u.z * position.z;
-	mat.m[14] = f.x * position.x + f.y * position.y + f.z * position.z;
+	matrix.m[3][0] = -r.x * position.x - r.y * position.y - r.z * position.z;
+	matrix.m[3][1] = -u.x * position.x - u.y * position.y - u.z * position.z;
+	matrix.m[3][2] = f.x * position.x + f.y * position.y + f.z * position.z;
 
-	return mat;
+	return matrix;
+}
+
+void translationMatrix(mat4 matrix, vec3 translation) {
+	matrix.m[3][0] = translation.x;
+	matrix.m[3][1] = translation.y;
+	matrix.m[3][2] = translation.z;
+}
+
+void rotationMatrix(mat4 matrix, vec3 rotation) {
+	float cp = cos(rotation.x);
+	float sp = sin(rotation.x);
+	float cy = cos(rotation.y);
+	float sy = sin(rotation.y);
+	float cr = cos(rotation.z);
+	float sr = sin(rotation.z);
+
+	matrix.m[0][0] = cr * cy;
+	matrix.m[0][1] = cr * sy * sp - sr * cp;
+	matrix.m[0][2] = cr * sy * cp + sr * sp;
+	matrix.m[1][0] = sr * cy;
+	matrix.m[1][1] = sr * sy * sp + cr * cp;
+	matrix.m[1][2] = sr * sy * cp - cr * sp;
+	matrix.m[2][0] = -sy;
+	matrix.m[2][1] = cy * sp;
+	matrix.m[2][2] = cy * cp;
+}
+
+void scaleMatrix(mat4 matrix, vec3 scale) {
+	matrix.m[0][0] *= scale.x;
+	matrix.m[1][1] *= scale.y;
+	matrix.m[2][2] *= scale.z;
+}
+
+mat4 generateTransformationMatrix(vec3 pos, vec3 rot, vec3 scl) {
+	mat4 matrix = getIdentity();
+	translationMatrix(matrix, pos);
+	rotationMatrix(matrix, rot);
+	scaleMatrix(matrix, scl);
+	return matrix;
 }
 
 vec3 vec3_add(vec3 a, vec3 b) {
 	return (vec3){a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+vec3 vec3_subtract(vec3 v1, vec3 v2) {
+	vec3 result;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
+	return result;
 }
 
 vec3 vec3_scale(vec3 v, float s) {
@@ -50,14 +105,6 @@ vec3 vec3_scale(vec3 v, float s) {
 
 vec3 vec3_lerp(vec3 a, vec3 b, float t) {
 	return vec3_add(vec3_scale(a, 1 - t), vec3_scale(b, t));
-}
-
-vec3 substracteVector(vec3 v1, vec3 v2) {
-	vec3 result;
-	result.x = v1.x - v2.x;
-	result.y = v1.y - v2.y;
-	result.z = v1.z - v2.z;
-	return result;
 }
 
 vec3 normalize(vec3 v) {
@@ -77,14 +124,18 @@ vec3 crossProduct(vec3 v1, vec3 v2) {
 	return result;
 }
 
+float length(vec3 p) {
+	return sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+}
+
 float lerp(float a, float b, float t) {
 	return a + (t) * (b - a);
 }
 
-float gaussianBulge(float x, float y, float A, float sigma) {
-	return A * exp(-(x*x + y*y) / (2 * sigma * sigma));
+float degreesToRadians(float degrees) {
+	return degrees * (M_PI / 180);
 }
 
-float length(vec3 p) {
-	return sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+float gaussianBulge(float x, float y, float A, float sigma) {
+	return A * exp(-(x*x + y*y) / (2 * sigma * sigma));
 }
--- geometry.h
+++ geometry.h
@@ -18,19 +18,25 @@ typedef struct vec3_s {
 } vec3;
 
 typedef struct {
-	float m[16];
+	float m[4][4];
 } mat4;
 
+mat4 getIdentity();
 mat4 projectionMatrix(float fov, float aspectRatio, float nearPlane, float farPlane);
 mat4 viewMatrix(vec3 position, vec3 focus, vec3 up);
+void translationMatrix(mat4 matrix, vec3 translation);
+void rotationMatrix(mat4 matrix, vec3 rotation);
+void scaleMatrix(mat4 matrix, vec3 scale);
+mat4 generateTransformationMatrix(vec3 pos, vec3 rot, vec3 scl);
 
 vec3 vec3_add(vec3 a, vec3 b);
+vec3 vec3_subtract(vec3 v1, vec3 v2);
 vec3 vec3_scale(vec3 v, float s);
 vec3 vec3_lerp(vec3 a, vec3 v, float t);
-vec3 substracteVector(vec3 v1, vec3 v2);
 vec3 normalize(vec3 v);
 vec3 crossProduct(vec3 v1, vec3 v2);
 float length(vec3 p);
 
 float lerp(float a, float b, float t);
+float degreesToRadians(float degrees);
 float gaussianBulge(float x, float y, float A, float sigma);
